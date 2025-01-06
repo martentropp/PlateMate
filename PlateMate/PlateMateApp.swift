bfaa3c91e326e45ce437a4485eecb69b071ab095
marten720 <137708101+marten720@users.noreply.github.com>
2025-01-06 13:32:25 +0200

    Initial Commit

---
+++ PlateMate/PlateMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateMateApp.swift
+//  PlateMate
+//
+//  Created by Märten Tropp on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PlateMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlateMateTests/PlateMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlateMateTests.swift
+//  PlateMateTests
+//
+//  Created by Märten Tropp on 1/6/25.
+//
+
+import Testing
+@testable import PlateMate
+
+struct PlateMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
